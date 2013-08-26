945a86aebcbf714c2c8876309069b08186223575
Raine Virta <raine.virta@gmail.com>
2013-08-26 21:49:05 +0300

    add devhub gallery

--- site/js/resume.js
+++ site/js/resume.js
@@ -1,13 +1,50 @@
 $(function() {
-	$(".fancybox")
-		.attr('rel', 'gallery')
-		.fancybox({
-			padding: 0,
-			helpers: {
-				thumbs: {
-					width: 75,
-					height: 75
-				}
+	var fancyboxOpts = {
+		padding: 0,
+		helpers: {
+			thumbs: {
+				width  : 75,
+				height : 75
+			},
+			title: {
+				type: 'outside'
 			}
-		});
+		}
+	};
+
+	var devhubTitle = "Various screenshots taken during development";
+
+	$("#fancybox-devhub").click(function() {
+		$.fancybox([
+			{
+				href  : 'images/devhub1.png',
+				title : devhubTitle
+			},
+			{
+				href  : 'images/devhub2.png',
+				title : devhubTitle
+			},
+			{
+				href  : 'images/devhub3.png',
+				title : devhubTitle
+			},
+			{
+				href  : 'images/devhub4.png',
+				title : devhubTitle
+			}
+		], fancyboxOpts);
+	});
+
+	$("#fancybox-memrise-button").click(function() {
+		$.fancybox([
+			{
+				href  : 'images/memrise-button1.png',
+				title : 'Promotional image for Chrome Store'
+			},
+			{
+				href  : 'images/memrise-button2.png',
+				title : 'Options page'
+			}
+		], fancyboxOpts);
+	});
 });
