7391abd7b632196f0f796a399f05d994eba340a1
Raine Virta <raine.virta@gmail.com>
2013-08-27 14:02:13 +0300

    remove comment

--- site/js/jquery.fancybox-thumbs.js
+++ site/js/jquery.fancybox-thumbs.js
@@ -1,19 +1,3 @@
- /*!
- * Thumbnail helper for fancyBox
- * version: 1.0.7 (Mon, 01 Oct 2012)
- * @requires fancyBox v2.0 or later
- *
- * Usage:
- *     $(".fancybox").fancybox({
- *         helpers : {
- *             thumbs: {
- *                 width  : 50,
- *                 height : 50
- *             }
- *         }
- *     });
- *
- */
 (function ($) {
 	//Shortcut for fancyBox object
 	var F = $.fancybox;
