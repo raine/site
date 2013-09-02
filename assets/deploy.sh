ebc1bec455a90c751614b1fb77d5d5b93f307e0d
Raine Virta <raine.virta@gmail.com>
2013-09-02 20:06:43 +0300

    add the dropbox deploy script

---
+++ assets/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ARG=$1
+set -x verbose #echo on
+
+if [ -z "$ARG" ]; then
+  TARGET=$(basename $(pwd))
+else
+  TARGET=$ARG
+fi
+
+rsync \
+  --delete-excluded \
+  --exclude $(basename $0) \
+  --exclude '.git*' \
+  --exclude 'node_modules/' \
+  -va output/ ~/Dropbox/Public/$TARGET
+
+FILE_TO_LINK="index.html"
+DROPBOX_ID=1103994
+URL="http://dl.dropboxusercontent.com/u/$DROPBOX_ID/$TARGET/$FILE_TO_LINK"
+
+# Copy public link to the clipboard
+echo -n $URL | pbcopy
+
+# Open the directory in Finder
+# open ~/Dropbox/Public/$TARGET
