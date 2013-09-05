4ab6b6293bfc64c30a3909411e1828556fbb1905
Raine Virta <raine.virta@gmail.com>
2013-09-05 04:41:29 +0300

    minify javascript with uglifier in nanoc

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,10 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+def all_js(files)
+  files.map do |file|
+    item = @items.find { |i| i.identifier == "/js/#{file}/" }
+    puts "File #{file} doesn't exist!" unless item
+    item.compiled_content
+  end.join("\n")
+end
