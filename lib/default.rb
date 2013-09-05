01eea41505db9cd122af6d8c314a8d571e8ff7a0
Raine Virta <raine.virta@gmail.com>
2013-09-05 08:52:02 +0300

    minify css with rainpress inside nanoc

--- lib/default.rb
+++ lib/default.rb
@@ -1,12 +1,20 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
-def all_js(files)
+def get_files(files, path)
   files.map do |file|
-    item = @items.find { |i| i.identifier == "/js/#{file}/" }
+    item = @items.find { |i| i.identifier == "/#{path}/#{file}/" }
     puts "File #{file} doesn't exist!" unless item
     item.compiled_content
   end.join("\n")
+end
+
+def all_js(files)
+  get_files files, :js
+end
+
+def all_css(files)
+  get_files files, :css
 end
 
 def production
